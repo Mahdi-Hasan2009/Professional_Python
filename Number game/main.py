e87991f34db34793d7855fbca95789301a2deb9b
Mahdi-HAsan2009 <2007m7t@gmail.com>
2024-08-29 14:04:56 +0600

    Number game

---
+++ Number game/main.py
@@ -0,0 +1,17 @@
+import random
+#Importing Module
+playing = True
+number = str(random.randint(10,20))
+
+print("I will generate a random number from 0 to 9, and you have to guess the number one digit at a time.")
+print("The game ends when you get 1 hero!")
+#Initiate loop till the condition is true
+while playing:
+  guess = input("Give me you best guess! \n")
+  if number == guess:
+    print("You win the game")
+    print(f"The number was {number}")
+    break
+  
+  else:
+    print("Your guess isn't right. Try again. \n")
