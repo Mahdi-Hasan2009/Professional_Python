277c3fb05a6911e88e71920cfbd7bfcf07bcdbc6
Mahdi-HAsan2009 <2007m7t@gmail.com>
2024-08-29 14:22:31 +0600

    Mathematical operations

---
+++ Mathematical operations/main.py
@@ -0,0 +1,8 @@
+import math
+print(f"The floor and ceiling value of 23.56 are {str(math.ceil(23.56))} {str(math.floor(23.56))}")
+
+x = 10
+y = -15
+print(f"The value of x after copying th sin from y is: {str(math.copysign(x,y))}")
+print(f"Absolute value of -96 and 56 are: {str(math.fabs(-96))}, {str(math.fabs(56))}")
+print(f"The GCD of 24 and 56: {str(math.gcd(24, 56))}")
