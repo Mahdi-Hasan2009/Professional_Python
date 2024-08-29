36c0bb4c35518b545d0aba8b0d0b670ac562d166
Mahdi-HAsan2009 <2007m7t@gmail.com>
2024-08-29 14:05:32 +0600

    Number game

--- Number game/main.py
+++ Number game/main.py
@@ -1,7 +1,7 @@
 import random
 #Importing Module
 playing = True
-number = str(random.randint(10,20))
+number = str(random.randint(0,9))
 
 print("I will generate a random number from 0 to 9, and you have to guess the number one digit at a time.")
 print("The game ends when you get 1 hero!")
