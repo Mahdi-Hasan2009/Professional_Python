aebf2165fefc1cbb909dcebb89e1020012de9f83
Mahdi-HAsan2009 <2007m7t@gmail.com>
2024-09-04 14:06:55 +0600

    Lists

---
+++ Lists/main.py
@@ -0,0 +1,16 @@
+#Create an empty list
+empty_list = []
+print()
+
+#A list of numbers
+numbers = [1, 2 , 3 , 4 , 5]
+print(numbers)
+
+#Use of * operator
+triples = [1,2,3] * 3
+print(triples)
+
+#Reverse the given string
+aList = [100, 200, 300, 400, 500]
+aList = aList[::-1]
+print(aList,"\n")
