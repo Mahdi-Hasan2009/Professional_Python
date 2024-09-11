649aa89d2b92727ecaeda7ca6c39f047d4ffe969
Mahdi-HAsan2009 <2007m7t@gmail.com>
2024-09-11 15:32:25 +0600

    Check the frequency HW

---
+++ Check the frequency HW/main.py
@@ -0,0 +1,19 @@
+# Initialize dictionary 
+test_dict = {'Codingal': 2, 'is': 2, 'best': 2, 'for': 2, 'Coding': 1}
+
+# Printing original dictionary
+print("The original dictionary:" + str(test_dict))
+
+# Initialize value 
+K = int(input("Write about your wanted frequency : "))
+
+# Using loop
+# Selective key values in dictionary
+res = 0
+for key in test_dict: 
+  if test_dict[key] == K: 
+    res = res + 1
+
+# printing result
+
+print("Frequency of K is: " + str(res))
