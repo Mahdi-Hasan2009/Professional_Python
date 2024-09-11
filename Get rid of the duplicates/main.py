0e94b537383c2ad1c997de5c667de6663e2c090c
Mahdi-HAsan2009 <2007m7t@gmail.com>
2024-09-11 14:20:25 +0600

    Get the country code

--- Get rid of the duplicates/main.py
+++ Get rid of the duplicates/main.py
@@ -1,18 +1,13 @@
-# Initialize dictionary
-test_dictionary = {'Codingal' : 2, 'is' : 2, 'for': 2, 'Coding' : 1}
+country_code = {
+  'India'     : '0091',
+  'Australia' : '0025',
+  'Nepal'     : '0091',
+}
 
-# Printing original dictionary
-print(f"The original dictionary :{str(test_dictionary)}")
+# Search dictionary for country  code of India
+print("Country code for India - ")
+print(country_code.get('India', 'Not Found'))
 
-# Initialize value
-K = 2
-
-# Using loop
-# Selective key values in dictionary
-res = 0
-for key in test_dictionary:
-  if test_dictionary[key] == K:
-    res = res + 1
-    
-# Printing result
-print(f"Frequency of K is : {str(res)}")
+# Search dictionary for country  code of Japan
+print("Country code for Japan - ")
+print(country_code.get('Japan', 'Not Found'))
