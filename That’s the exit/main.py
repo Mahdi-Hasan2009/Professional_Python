7af1eb8b5bb3fa7184785e57579dd951ab19f31c
Mahdi-HAsan2009 <2007m7t@gmail.com>
2024-09-17 14:19:05 +0600

    That’s the exit

---
+++ That’s the exit/main.py
@@ -0,0 +1,11 @@
+# Python programme to demonstrate
+# exit()
+for i in range(10):
+  # If the value of i becomes
+  # 5 then the programme is forced
+  # to exit
+  if i == 5:
+    # Prints the exit message
+    print(exit)
+    exit()
+  print(i)
