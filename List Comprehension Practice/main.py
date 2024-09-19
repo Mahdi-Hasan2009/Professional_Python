43fb9210660f666b6d7d789f866916fdd3496886
Mahdi-HAsan2009 <2007m7t@gmail.com>
2024-09-19 13:49:35 +0600

    List Comprehension Practice

--- List Comprehension Practice/main.py
+++ List Comprehension Practice/main.py
@@ -4,7 +4,7 @@ odd_list =[i for i in range(num) if i % 2 != 0]
 print(f"List of odd numbers: {odd_list} \n")
 
 even_list =[i for i in range(num) if i % 2 == 0]
-print(f"List of even numbers: {odd_list} \n")
+print(f"List of even numbers: {even_list} \n")
 
 fruits = ['apple', 'banana', 'mango', 'papaya', 'grapes']
 Fruits = [x[0].upper() + x[1:] for x in fruits]
