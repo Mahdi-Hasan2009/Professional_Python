42c7188cf901959f07a5366c7d20a54ae0078ebd
Mahdi-HAsan2009 <2007m7t@gmail.com>
2024-09-19 14:40:10 +0600

    Tic Tac Toe Game

--- Tic Tac Toe Game/main.py
+++ Tic Tac Toe Game/main.py
@@ -2,7 +2,7 @@
 
 ''' We will make t+he board using dictionary 
     in which keys will be the location(i.e : top-left,mid-right,etc.)
-    and initialliy it's values will be empty space and then after every move 
+    and initially it's values will be empty space and then after every move 
     we will change the value according to player's choice of move. '''
 
 theBoard = {'7': ' ' , '8': ' ' , '9': ' ' ,
@@ -16,7 +16,7 @@ for key in theBoard:
 
 ''' We will have to print the updated board after every move in the game and 
     thus we will make a function in which we'll define the printBoard function
-    so that we can easily print the board everytime by calling this function. '''
+    so that we can easily print the board every time by calling this function. '''
 
 def printBoard(board):
     print(board['7'] + '|' + board['8'] + '|' + board['9'])
@@ -25,7 +25,7 @@ def printBoard(board):
     print('-+-+-')
     print(board['1'] + '|' + board['2'] + '|' + board['3'])
 
-# Now we'll write the main function which has all the gameplay functionality.
+# Now we'll write the main function which has all the game play functionality.
 def game():
 
     turn = 'X'
