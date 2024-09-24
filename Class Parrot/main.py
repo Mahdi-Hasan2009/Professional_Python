53eac6d788738dd55f3df10039019f8f90d7b546
Mahdi-HAsan2009 <2007m7t@gmail.com>
2024-09-24 14:26:44 +0600

    Class Parrot

---
+++ Class Parrot/main.py
@@ -0,0 +1,17 @@
+# Create class
+class Parrot:
+  # Class attribute
+  species = "bird"
+  # Instance attribute
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+# Instantiate the Class Parrot
+blu = Parrot("Blu", 10)
+woo = Parrot("woo", 15)
+# Access the class attributes
+print("Blu is a {}".format(blu.species))
+print("Woo is also a {}".format(woo.species))
+# Access the instance attributes
+print()
+print()
