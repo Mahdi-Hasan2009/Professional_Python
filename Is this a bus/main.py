19a9e88c1593f062bb640f3468a0e6b5f8c8dd8a
Mahdi-HAsan2009 <2007m7t@gmail.com>
2024-09-30 13:59:03 +0600

    Is this a bus?

---
+++ Is this a bus/main.py
@@ -0,0 +1,11 @@
+# Parent element
+class Vehicle:
+  def __init__(self, name, max_speed, mileage):
+    self.name = name
+    self.max_speed =max_speed
+    self.mileage = mileage
+# Child Element
+class Bus(Vehicle):
+  pass
+School_bus = Bus("School Volvo", 180, 12)
+print(f"Vehicle Name: {School_bus.name} Speed:{School_bus.max_speed} Mileage:{School_bus.mileage}")
