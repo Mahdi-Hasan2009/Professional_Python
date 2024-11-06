c3f0f23943fc243b2de12a251787eed54febcfd3
Mahdi-HAsan2009 <2007m7t@gmail.com>
2024-11-06 14:10:19 +0600

    Tkinter Window

---
+++ Tkinter/001. Tkinter Window/main.py
@@ -0,0 +1,11 @@
+# Import necessary libraries
+from tkinter import *
+
+# Create Window
+window = Tk()# tk is the function to create the window
+
+# Set the window Title and Geometry
+window.title("'Demo Window")
+window.geometry('400x300')
+
+window.mainloop()# The soul of the programme as it displays
