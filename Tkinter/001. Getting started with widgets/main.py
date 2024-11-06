3854de718b5dae7f2d12cd24ce8b5d4087f6905d
Mahdi-HAsan2009 <2007m7t@gmail.com>
2024-11-06 15:07:06 +0600

    Getting started with widgets

--- Tkinter/001. Getting started with widgets/main.py
+++ Tkinter/001. Getting started with widgets/main.py
@@ -33,3 +33,31 @@ def display():
   # to make it accessible anywhere in the programme
   global message
   message = "Welcome to the Application! \nToday's date is: "
+  greet = "Hello "+name+"\n"
+  
+  # Display details in a text box
+  # Specify where to add details inside the text box
+  text_box.insert(END, greet)
+  text_box.insert(END, message)
+  text_box.insert(END, date.today())
+  
+# Add a Text Width display information/message
+text_box = Text(height=3)
+
+# Add button and give value of command as name of the function
+# Press button, display function will be called automatically
+btn = Button(
+  text = "Begin",
+  command=display,
+  height=1,
+  bg="#1261A0",
+  fg='white'
+)
+
+# Organize all the widgets in the window
+lbl.pack()
+name_lbl.pack()
+name_entry.pack()
+btn.pack()
+text_box.pack()
+root.mainloop()
