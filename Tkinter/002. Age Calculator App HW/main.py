d34a849afaa5ac1b8a00c299216c8f9f3b09d461
Mahdi-HAsan2009 <2007m7t@gmail.com>
2024-11-12 19:32:10 +0600

    Age Calculator App

---
+++ Tkinter/002. Age Calculator App HW/main.py
@@ -0,0 +1,15 @@
+# Import necessary libraries
+from tkinter import *
+import datetime
+
+# Create Window
+root = Tk()
+root.title('Age Calculator App')
+root.geometry('400x400')
+
+# Create a frame to organize elements better
+frame = Frame(master=root, height=200, width=360, bg="#FAB072")
+
+# Add widgets
+# Add Label
+lbl1 = Label()
