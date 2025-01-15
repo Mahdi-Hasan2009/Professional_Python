a0e13ce937513afe0879546e17528d9a9e5f79b5
Mahdi-HAsan2009 <2007m7t@gmail.com>
2025-01-15 13:20:53 +0600

    O(1)Time

---
+++ Data_Science/008. O(1)Time/main.py
@@ -0,0 +1,11 @@
+# Program to show Constant Time Complexity
+
+def print_number(n):
+  iteration = 0
+  print("The number entered by user is  ",n)
+  iteration+=1
+  print("Total  iterations done by the code is ", iteration, "\n")
+  
+print_number(10)
+print_number(20)
+print('\n With any \'n\' the time taken by our code won\'t change')
