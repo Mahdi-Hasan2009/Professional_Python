976f3b962570e1561f5b270c568e1a38a44e9d99
Mahdi-HAsan2009 <2007m7t@gmail.com>
2025-01-15 13:26:04 +0600

    O(n)Time

---
+++ Data_Science/008. O(n)Time/main.py
@@ -0,0 +1,12 @@
+def OnTime(n):
+  iterations = 0
+  for i in range(1, n+1):
+    iterations+=1
+  print(f'When\'n\' is {n} Iterations = {iterations}')
+  
+
+OnTime(10)
+OnTime(12)
+OnTime(33)
+OnTime(40)
+OnTime(50)
