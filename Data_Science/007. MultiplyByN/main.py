0d32f17838f8c6edb976d8f4605f97205f2afa72
Mahdi-HAsan2009 <2007m7t@gmail.com>
2025-01-15 11:54:42 +0600

    MultiplyByN

--- Data_Science/007. MultiplyByN/main.py
+++ Data_Science/007. MultiplyByN/main.py
@@ -4,8 +4,15 @@ def o1(n, m):
   total = n * m
   print(f'Result is: {total}\n1 iteration: 1')
   
+# For N iteration run a loop
+def oN(n,m):
+  total = 0
+  for i in range(1, n + 1):
+    total +=m
+  print(f'N iteration: {n}')
   
 m = int(input('Enter \'a\' for a*b : '))
 n = int(input('Enter \'b\' for a*b : '))
 
 o1(m,n)
+oN(m,n)
