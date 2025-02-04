772f0c5f6cb2e83326e32d9e5903ef737919d02e
Mahdi-HAsan2009 <2007m7t@gmail.com>
2025-02-04 15:10:05 +0600

    Bit Resaerch

---
+++ Data_Science/013. HW Bit Research/main.py
@@ -0,0 +1,25 @@
+# Program to showcase working of different types of bitwise operators
+
+num1 = 9
+num2 = 5
+
+# Using AND operator
+print("num1 & num2 =", num1 & num2)
+
+# Using OR operator
+print("\nnum1 | num2 =", num1 | num2)
+
+# Using NOT operators
+print("\n~num1 =", ~num1)
+
+# Using XOR operator
+print("\nnum1 ^ num2 =", num1 ^ num2)
+
+num1 = 10
+num2 = 4
+
+# Using Left Shift operator on num1
+print("\nnum1 << 1 =", num1 << 1)
+
+# Using Right Shift operator on num2 from 2 position
+print("\nnum2 >> 2  =", num2  << 2)
