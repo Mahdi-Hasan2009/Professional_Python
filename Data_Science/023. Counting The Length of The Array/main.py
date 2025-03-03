c1fd47bf1335e2974f33f916553d7f5f42f5ad7d
Mahdi-HAsan2009 <2007m7t@gmail.com>
2025-03-03 14:12:54 +0600

    Array Length Recursive

--- Data_Science/023. Counting The Length of The Array/main.py
+++ Data_Science/023. Counting The Length of The Array/main.py
@@ -5,15 +5,15 @@ def length_of_array(a):
     return 0
   return 1 + length_of_array(a[1::2]) + length_of_array(a[2::2])
 
-length = int(input("Enter the length of the array: "))
+length = int(input("\033[96mEnter the length of the array: \033[0m"))
 a = []
 b = 1
 
 def make_array(length, b):
   if length == 0:
     return
-  a.append(int(input(f"Enter the element of {b} position: ")))
+  a.append(int(input(f"\033[31mEnter the element of {b} position: \033[0m")))
   make_array(length - 1, b + 1)
 
 make_array(length, b)
-print(f"Your array is: {a}\nLength of the array is:  {length_of_array(a)}")
+print(f"\033[33mYour array is: {a}\033[0m\n\033[32mLength of the array is:  {length_of_array(a)}\033[0m")
