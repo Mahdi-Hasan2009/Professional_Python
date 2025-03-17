6bd4cbc3d5c2d23358b871314cd801cbca170334
Mahdi-HAsan2009 <2007m7t@gmail.com>
2025-03-17 13:49:27 +0600

    Rat maze

--- Data_Science/026. Rat_maze/main.py
+++ Data_Science/026. Rat_maze/main.py
@@ -9,7 +9,7 @@ def rat_maze(maze, i, j, m, n , arr):
   if arr[-1][-1] == 1:
     return True
   if valid__path(maze, i, j, m, n):
-    arr[i][j] =  1
+    arr[i][j] =  1 # row and column number
     if rat_maze(maze, i + 1, j, m, n, arr):
       return True
     if rat_maze(maze, i, j+1, m, n , arr):
