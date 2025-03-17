5c7cc739b9a76c7ee03ede958dc1777fb83ad22b
Mahdi-HAsan2009 <2007m7t@gmail.com>
2025-03-17 14:23:21 +0600

    SecondLargestElement

---
+++ Data_Science/027. SecondLargestElement/main.py
@@ -0,0 +1,15 @@
+def print2LArgest(a, arr_size):
+  largest = secondLargest = -21515551515512200
+  for i in range(arr_size):
+    if a[i]>largest:
+      secondLargest = largest
+      largest = a[i]
+    elif a[i]>secondLargest and a[i] != largest:
+      secondLargest = a[i]
+  
+  print(secondLargest)
+
+arr = [191, 84, 140, 45, 74, 74, 47, 17, 8, 12]
+arr_size = len(arr)\
+
+print2LArgest(arr, arr_size)
